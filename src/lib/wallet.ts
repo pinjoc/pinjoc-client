facff41682b6dcfa64ff1731ceb506d475a3b199
Evriyana Indra Saputra <eindrasap@gmail.com>
2025-02-26 23:04:50 +0700

    feat: add config for anvil network work

--- src/lib/wallet.ts
+++ src/lib/wallet.ts
@@ -18,9 +18,24 @@ const connectors = connectorsForWallets(
 	},
 );
 
+const localhost = {
+	id: 31337, // Chain ID untuk Anvil / Hardhat
+	name: "Localhost",
+	network: "localhost",
+	nativeCurrency: {
+		name: "Ethereum",
+		symbol: "ETH",
+		decimals: 18,
+	},
+	rpcUrls: {
+		default: { http: ["http://127.0.0.1:8545"] },
+		public: { http: ["http://127.0.0.1:8545"] },
+	},
+};
+
 export const config = createConfig({
 	connectors,
-	chains: [mainnet],
+	chains: [mainnet, localhost],
 	transports: {
 		[mainnet.id]: http(),
 	},
